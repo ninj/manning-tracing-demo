55b30b0545cd22d172cc2841996f888ede9812ff
Steve Wong <swong@metahq.com>
2024-01-17 23:36:21 +0000

    feat: add jaeger client

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,12 @@
-FROM openjdk:17-jdk-alpine
+FROM azul/zulu-openjdk-alpine:8-jre-headless
 COPY target/tracing-demo-0.0.1-SNAPSHOT.jar /tracing-demo-0.0.1-SNAPSHOT.jar
+
+# For MacOS, comment this line if you're using Linux
+ENV JAEGER_ENDPOINT="http://host.docker.internal:14268/api/traces"
+# For Linux, uncomment this line if you're using Linux
+# ENV JAEGER_ENDPOINT="http://127.0.0.1:14268/api/traces"
+ENV JAEGER_SAMPLER_TYPE=const
+ENV JAEGER_SAMPLER_PARAM=1
+ENV JAEGER_REPORTER_LOG_SPANS=true
+
 ENTRYPOINT ["java","-jar","/tracing-demo-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/example/tracingdemo/EShopController.java
+++ src/main/java/com/example/tracingdemo/EShopController.java
@@ -1,13 +1,28 @@
 package com.example.tracingdemo;
 
+import io.jaegertracing.Configuration;
+import io.jaegertracing.internal.JaegerTracer;
+import io.opentracing.Span;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 @RestController
 public class EShopController {
 
+    private JaegerTracer tracer;
+
+    @PostConstruct
+    public void init() {
+        tracer = Configuration.fromEnv("EShop").getTracer();
+    }
+
     @RequestMapping("/checkout")
     public String checkout() {
-        return "You have successfully checked out your shopping cart.";
+        Span span = tracer.buildSpan("checkout").start();
+        String result = "You have successfully checked out your shopping cart.";
+        span.finish();
+        return result;
     }
 }
